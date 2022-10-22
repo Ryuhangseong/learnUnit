e51bb15fca25e129d54292002c57ff924f790e4a
ryuhangseong <liuhangcheng2002@gmail.com>
2022-10-22 11:48:09 +0800

    0.0.4 version

--- test_search.py
+++ test_search.py
@@ -10,14 +10,13 @@ Copyright (c) 2022 by ryuhangseong liuhangcheng2002@gmail.com, All Rights Reserv
 '''
 
 
-
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 from ddt import ddt, file_data
 
 import unittest
@@ -49,6 +48,7 @@ class TestSearch(unittest.TestCase):
             if handle != handle_window_index:
                 self.driver.switch_to.window(handle)
                 print("Now Register Window!")
+        print('\n')
 
     def test_page_2_testclass(self):
         url_second = "https://www.testclass.cn/"
@@ -66,6 +66,7 @@ class TestSearch(unittest.TestCase):
         sleep(.5)
         self.driver.set_window_rect(0, 0, 1024, 768)
         sleep(1)
+        print('\n')
 
     @file_data("./data/test_page_3_baidu.yaml")
     def test_page_3_baidu(self, **kwargs):
@@ -93,9 +94,47 @@ class TestSearch(unittest.TestCase):
         self.driver.switch_to.window(window_index_handle)
         loc_index_iframe_close_button = (By.ID, "TANGRAM__PSP_4__closeBtn")
         WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable(loc_index_iframe_close_button))
-        sleep(1)
         self.driver.find_element('id', 'TANGRAM__PSP_4__closeBtn').click()
+        js_set_setting_visible = "document.getElementById('s-user-setting-menu').style.display='block';"
+        js_set_setting_invisible = "document.getElementById('s-user-setting-menu').style.display='none';"
         sleep(2)
+        self.driver.execute_script(js_set_setting_visible)
+        sleep(2)
+        self.driver.execute_script(js_set_setting_invisible)
+        sleep(2)
+        ActionChains(self.driver).move_to_element(self.driver.find_element('id', 's-usersetting-top')).perform()
+        sleep(2)
+        print('\n')
+
+    @file_data('./data/test_page_4_huawei.yaml')
+    def test_page_4_huawei(self, **kwargs):
+        huawei = kwargs['huawei']
+        url_baidu = huawei['url_baidu']
+        loc_baidu_search_button = (By.ID, "su")
+        loc_baidu_search_input = (By.ID, "kw")
+        loc_baidu_result_container = (By.ID, "container")
+        self.driver.get(url_baidu)
+        self.driver.minimize_window()
+        self.assertEqual(self.driver.current_url, huawei['url_baidu'])
+        self.driver.set_window_position(125, 125)
+        self.driver.set_window_size(800, 600)
+        self.driver.set_window_rect(0, 0, 1152, 864)
+        self.driver.maximize_window()
+        self.driver.fullscreen_window()
+        WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable(loc_baidu_search_button))
+        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(loc_baidu_search_input))
+        handle_index_baidu = self.driver.current_window_handle
+        title_index_baidu = self.driver.title
+        self.driver.find_element("id", "kw").send_keys("华为")
+        self.driver.find_element("id", "su").click()
+        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(loc_baidu_result_container))
+        self.driver.back()
+        WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable(loc_baidu_search_button))
+        title_index_baidu_back = self.driver.title
+        self.assertEqual(title_index_baidu, title_index_baidu_back)
+        self.driver.forward()
+        handle_result_baidu = self.driver.current_window_handle
+        print('\n')
 
 
 if __name__ == '__main__':
